f3e0bce844a6a5d39d7b8a3de60e10f24dbc49e9
pdanner <paul.j.danner@gmail.com>
2014-09-30 15:03:08 -0500

    Update to Add networking to shots

--- Assets/Scripts/WeaponScript.cs
+++ Assets/Scripts/WeaponScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WeaponScript : MonoBehaviour
+public class WeaponScript : Photon.MonoBehaviour
 {
 	public Transform shotPrefab;
 
@@ -22,27 +22,35 @@ public class WeaponScript : MonoBehaviour
 		}
 	}
 
+	// Fire off a round!
+	[RPC]
+	void FireOneShot()
+	{
+		GameObject shotTransform = Instantiate (Resources.Load("Shot"), transform.position, transform.rotation) as GameObject;
+		//shotTransform.position = transform.position + new Vector3(4f, -0.3f, 0);
+		ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
+		if (shot != null)
+		{
+			shot.isEnemyShot = false; //	isEnemy;
+		}
+		
+		MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
+		if (move != null)
+		{
+			move.direction = this.transform.right; 
+		}
+	}
 	public void Attack(bool isEnemy)
 	{
 		if (CanAttack)
 		{
 			shootCooldown = shootingRate;
+			this.photonView.RPC ("FireOneShot", PhotonTargets.All, null);
+			//var shotTransform = Instantiate(shotPrefab) as Transform;
 
-			var shotTransform = Instantiate(shotPrefab) as Transform;
+			//shotTransform.position = transform.position + new Vector3(4f, -0.3f, 0);
 
-			shotTransform.position = transform.position + new Vector3(4f, -0.3f, 0);
-
-			ShotScript shot = shotTransform.gameObject.GetComponent<ShotScript>();
-			if (shot != null)
-			{
-				shot.isEnemyShot = isEnemy;
-			}
-
-			MoveScript move = shotTransform.gameObject.GetComponent<MoveScript>();
-			if (move != null)
-			{
-				move.direction = this.transform.right; 
-			}
+			//
 		}
 	}
 
