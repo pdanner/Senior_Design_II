8bcd6b7fbd6de11fd64193eb23e33ff1cf36c27a
pdanner <paul.j.danner@gmail.com>
2014-10-28 18:04:15 -0500

    Fixed Windows bullet glitch

---
+++ Assets/Scripts/PickPlayerScript.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class PickPlayerScript : Photon.MonoBehaviour {
+	
+	private GUISkin skin;
+
+	void Awake()
+	{
+		skin = Resources.Load ("GUISkin3") as GUISkin;
+	}
+	void OnGUI()
+	{
+		skin = Resources.Load ("GUISkinCY1") as GUISkin;
+		GUI.skin = skin;
+		if(GUI.Button (new Rect(2 * Screen.width/6 - 250, 2 * Screen.height/5, 250, 250), ""))
+		{
+			PhotonNetwork.playerName = "Player1";
+			PhotonNetwork.JoinRoom("myRoom");
+		}
+		skin = Resources.Load ("GUISkinCY2") as GUISkin;
+		GUI.skin = skin;
+		if(GUI.Button (new Rect(3 * Screen.width/6 - 250, 2 * Screen.height/5, 250, 250), ""))
+		{
+			PhotonNetwork.playerName = "Player2";
+			PhotonNetwork.JoinRoom("myRoom");
+		}
+		skin = Resources.Load ("GUISkinCY3") as GUISkin;
+		GUI.skin = skin;
+		if(GUI.Button (new Rect(4 * Screen.width/6 - 250, 2 * Screen.height/5, 250, 250), ""))
+		{
+			PhotonNetwork.playerName = "Player3";
+			PhotonNetwork.JoinRoom("myRoom");
+		}
+		skin = Resources.Load ("GUISkinCY4") as GUISkin;
+		GUI.skin = skin;
+		if(GUI.Button (new Rect(5 * Screen.width/6 - 250, 2 * Screen.height/5, 250, 250), ""))
+		{
+			PhotonNetwork.playerName = "Player4";
+			PhotonNetwork.JoinRoom("myRoom");
+		}
+	}
+}
+
+
